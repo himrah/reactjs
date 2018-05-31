7c793421d63d5bda356a84e1fbf31132e3847df4
himrah <rkrjob55@gmail.com>
2018-05-31 21:07:54 +0530

    update

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -106,7 +106,6 @@ class Edit extends React.Component{
     }
   }
   componentDidMount = () => {
-    
     //const user = fromJS({name:'ajay',contact:{mobile:90}})
     const user = fromJS(this.props.info.users) 
     console.log(this.props.info.users)
@@ -126,6 +125,7 @@ class Edit extends React.Component{
     /*this.setState({
       user:this.state.user
     })*/  
+    this.props.view(e)
     switch(e.target.name){
       case 'first_name':this.setState({user:user.set('firstName',e.target.value)});break;
       case 'last_name':this.setState({user:user.set('lastName',e.target.value)});break;
@@ -217,32 +217,58 @@ class Thumb extends React.Component{
 
 
 
-class Profile extends React.Component{
-  constructor(props) {
+class Single extends React.Component{
+  constructor(props){
     super(props);
-    this.state ={
-      //file:null
+    this.state={
       maxWidth:'33%',
       grid:3,
       isedtable:true,
       edit:'none',
+      user:{},      
     }
     this.onFormSubmit = this.onFormSubmit.bind(this)
     this.onChange = this.onChange.bind(this)
     this.fileUpload = this.fileUpload.bind(this)
+
+  }
+
+  componentDidMount = () => {
+    //console.log(this.props)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps)
+    if (nextProps.data.data.loading){
+      //console.log(nextProps)
+    }
+     else{
+       //console.log(nextProps.user) 
+       this.setState(
+        {
+          user:{
+            user_id:nextProps.user.id,
+            first_name:nextProps.user.firstName,
+            last_name:nextProps.user.lastName,
+            about:nextProps.user.profile.about,
+            dob:nextProps.user.profile.birthDay,
+            website:nextProps.user.profile.website,
+            twitter:nextProps.user.profile.twitter,
+            fb:nextProps.user.profile.fb,
+            instagram:nextProps.user.profile.instagram
+          }
+        })
+      }
+    //this.setState({user:'ajay'})
+  }
+
+
+  Updateuser(e){
+    console.log(e.target)
+    //console.log(this.props)
   }
   
-  componentDidMount = () => {
-    console.log(this.props)
-    this.setState(
-      {
-        grid:this.props.Gallery.grid,
-        maxWidth:this.props.Gallery.width
-      }
-    )
-    //this.setState()
-  }
-    
+
   onFormSubmit(e){
     e.preventDefault()
     this.fileUpload(this.state.file).then((response)=>{
@@ -269,19 +295,15 @@ class Profile extends React.Component{
         headers: {
             'content-type': 'multipart/form-data',
             'authorization': `JWT ${token}`,
-            
-            //'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            //'validateStatus':201,
           }
     }
-    //console.log(formData)
     return  post(url, formData,config)
 }
 changeWidth(e){
-
 this.props.dispatch(Gallery({grid:e,width:`${100/e}%`}))
 this.setState({'grid':e,'maxWidth':`${100/e}%`})
 }
+
 ShowEditInfo(){
   if (this.state.edit=='none')
   {
@@ -291,20 +313,19 @@ ShowEditInfo(){
     this.setState({edit:'none'})
   }
 }
-  render(){
 
-        let { data, mutate } = this.props
-        //console.log(data)
+
+render(){
+        console.log(this.props.data)
+        //console.log(this.state.user)
+        let { data, mutate } = this.props.data
         if (data.loading || !data.users) {
           return <div>Loading...</div>
         }
-
         const style={
           'borderRadius': '50%',
         }
-        const profile_photos = this.props.data.users.photos.edges;
-        //console.log(profile_photos)
-        //var l = []
+        const profile_photos = data.users.photos.edges;
         var photo_list=[]
         var counter = 0
         let l = profile_photos
@@ -316,8 +337,7 @@ ShowEditInfo(){
           photo_list.push(t)
           counter+=this.state.grid
         }
-        //console.log(this.props)
-        //console.log(photo_list)
+        console.log(this.state.user)
         return(
           <main className="main">      
           <Helmet>
@@ -365,37 +385,9 @@ ShowEditInfo(){
                   </div>
               </div>
 
-{/*}              <div className="info">
-                    { data.users.profile ?(
-                      <div className="personal">
-                          <div>
-                            username : {data.users.username}
-                          </div>
-                          <div>
-                            Name : {data.users.firstName + " " +data.users.lastName}
-                          </div>
-                          <div>
-                            Birthday : {data.users.profile.birthDay}
-                          </div>                                
-                      </div>
-                    ) : (
-                      <div className="personal">
-                      </div>
-                    )
-                    }
-                  
-                <div className="input">
-                  <form onSubmit={this.onFormSubmit}>
-                    <input type="file" onChange={this.onChange}/>
-                    <button type="submit">Upload</button>
-                  </form>
-                </div>  
-            </div>
-   */}
-
             </section>
             <section className="edit_info" style={{display:this.state.edit}}>
-              <Edit info={data} mutate = {mutate}/>
+              <Edit info={data} mutate = {mutate}  view={this.Updateuser.bind(this)}/>
               </section>
             <section className="slide">
                     <div className="fl_rw sl_sec">
@@ -416,13 +408,6 @@ ShowEditInfo(){
               </div>
             </section>
             <section className="bottom">
-            {/*
-              <div className="box">
-                <div className="group">
-                  {profile_photos.map(p=><Thumb key={p.id} photo={p} />)}
-                </div>
-              </div>
-            */}
             <div className="p_cont">
             {   
               <div className="_row">
@@ -434,19 +419,47 @@ ShowEditInfo(){
           </main>
         )
     }
+
+
+}
+
+
+
+
+class Profile extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state ={
+      //file:null
+      maxWidth:'33%',
+      grid:3,
+      isedtable:true,
+      edit:'none',
+      user:[],
+    }
+  }
+  
+  componentDidMount = () => {
+    this.setState(
+      {
+        grid:this.props.Gallery.grid,
+        maxWidth:this.props.Gallery.width
+      }
+    )
+    //this.setState()
+  }
+
+  render(){
+    return(
+      <Single data={this.props}   user={this.props.data.users}/>
+    )
+  }
+
 }
 
 
 class Group extends React.Component{
-  /*constructor(props){
-    super(props);
-    this.state={
-      'maxWidth':'33.3%'
-    }
-  }*/
   render(){
-    //console.log(this.props.photo)
-    //const mw = this.props.maxWidth
     return(
       <div className="fl_rw _group">
         {
@@ -471,160 +484,8 @@ export default compose(
   connect(
     (state)=>({
       Gallery:state.Gallery,
-      //onGallery:state.onGal
     })
   ),
   graphql(query,queryOptions),
   graphql(updateinfo)
 )(Profile)
-//export default comp(Profile)
-//export default graphql(query,queryOptions)(Profile)
-
-/*
-export default compose(
-  connect(mapStateToProps,mapDispatchToProps),
-  graphql(query, queryOptions),
-  //graphql(upload)
-)(Profile)
-*/
-//var a=this.props.match.params.userName
-
-/*const upload = gql`mutation abc(){
-  img(
-    )
-  {
-    formErrors
-  }
-}
-`*/
-
-
-//DetailView = graphql(query, queryOptions)(DetailView)
-//export default DetailView
-
-
-
-/*
-export default compose(
-  graphql(MY_QUERY),
-  graphql(UpdateComment)
-)(Article)
-Profile = graphql(query)(Profile)
-export default Profile;
-*/
-
-
-
-
-/*
-const query = gql`query user($username:String!){
-    users(username:$username){
-      id,
-      username
-      firstName,
-      lastName
-      photos{
-        edges{
-          node{
-            id,
-            thumbs
-            photo
-            originalPhoto
-          }
-        }
-      }
-      profile{
-        id
-        birthDay
-        profilePic{
-          id
-          profileThumbs
-        }
-      }
-    }
-  }
-
-
-*/
-
-
-
-
-
-
-/*  onSubmit(e){
-    e.preventDefault()
-    //console.log(this.props)
-    //const url = 'http://localhost:8000/api/post/';
-    //const formData = new FormData();
-    //formData.append('file',file)    
-    this.fileUpload(this.state.file).then((response)=>{
-      console.log(response.data);
-    }).catch(function(error){
-      console.log(error)
-    })    
-  }
-*/
-
-/*
-  fileUpload(file){
-    //console.log(file)
-    const url = 'http://localhost:8000/api/post/';
-    let formData = new FormData();
-    //formData.append('original_photo',file)
-    formData.append('file',file)
-    //formData.get("original_photo")
-    const token = localStorage.getItem('token')
-    const config = {
-        headers: {
-            'content-type': 'multipart/form-data',
-            'authorization': `JWT ${token}`, 
-            'Accept': 'application/json'
-        }
-    }
-    //console.log(formData.get("original_photo"))
-    return  post(url, formData.get('file'),config)
-    //return  post(url, formData.get("original_photo"),config)
-  }
-
-
-
-*/
-
-  /*profileupdate(file){
-    const url = 'http://localhost:8000/api/profilepic/';
-    const formData = new FormData();
-    formData.append('original_photo',file)
-    const token = localStorage.getItem('token')
-    const config = {
-        headers: {
-            'content-type': 'multipart/form-data',
-            'authorization': `JWT ${token}` 
-        }
-    }
-    return  put(url, formData,config)
-  }*/
-
-
-/*
-  onChange(e){
-    this.setState({file:e.target.files[0]})
-  }
-*/
-/*  onChangePost(e){
-    //this.setState({file:e.target.files[0]})
-    let file = e.target.files[0]
-    console.log(file)
-    const url = 'http://localhost:8000/api/profilepic/'+localStorage;
-    const formData = new FormData();
-    formData.append('original_photo',file)
-    const token = localStorage.getItem('token')
-    const config = {
-        headers: {
-            'content-type': 'multipart/form-data',
-            'authorization': `JWT ${token}` 
-        }
-    }
-    return  put(url, formData,config)
-  }*/
-
